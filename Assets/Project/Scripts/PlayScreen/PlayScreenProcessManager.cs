267521a838756a23a58cfcd0c7334e3723efbab5
smpny7 <symphonymode7@icloud.com>
2020-07-01 10:50:22 +0900

    [add] ノーツ落下タイミング調整

--- Assets/Project/Scripts/PlayScreen/PlayScreenProcessManager.cs
+++ Assets/Project/Scripts/PlayScreen/PlayScreenProcessManager.cs
@@ -1,4 +1,5 @@
-﻿// using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using UnityEditor;
 using UnityEngine;
 // using UnityEngine.SceneManagement;
 // using UnityEngine.UI;
@@ -7,23 +8,56 @@ using System.IO;
 // using System.Text;
 
 public class PlayScreenProcessManager : MonoBehaviour {
-    // public string csvFilePass = "CSV/burningHeart.csv";
-    // private int notesTotal = 0;
-    // private float[] _timing = { 0 };
-    // private int[] _lineNum = { 0 };
-    // void Start () {
-    //     LoadCSV ();
-    // }
+    // public string csvFilePass = "Assets/CSV/burningHeart.csv";
+    public GameObject[] Note;
+    private AudioSource _audioSource;
 
-    // void LoadCSV () {
-    //     TextAsset csv = Resources.Load (csvFilePass) as TextAsset; //ファイル読み込み
-    //     StringReader reader = new StringReader (csv.text); //CSV内容格納
-    //     while (reader.Peek () > -1) { //文字がある間
-    //         string line = reader.ReadLine (); //1行読み込み
-    //         string[] values = line.Split (','); //カンマで区切って配列に格納
-    //         _timing[notesTotal] = float.Parse (values[0]); //_timing[]配列に格納 (グローバル変数)
-    //         // _timing[notesTotal] = float.Parse (values[0]) + 1; //_timing[]配列に格納 (グローバル変数)
-    //         _lineNum[notesTotal++] = int.Parse (values[1]); //_lineNum[]配列に格納 (グローバル変数)
-    //     }
-    // }
+    private int notesTotal = 0;
+    private int _notesCount = 0;
+    private float _startTime = 0;
+    public float[] _timing;
+    public int[] _lineNum;
+
+    // ---------------------
+    private int timeOffset = 0;
+    // ---------------------
+
+    async void Start () {
+        _timing = new float[1024];
+        _lineNum = new int[1024];
+        LoadCSV ();
+        _audioSource = GameObject.Find ("Music").GetComponent<AudioSource> ();
+        _startTime = Time.time;
+        await Task.Delay (1950);
+        _audioSource.Play ();
+    }
+
+    void Update () {
+        CheckNextNotes ();
+    }
+
+    void LoadCSV () {
+        TextAsset csv = AssetDatabase.LoadAssetAtPath<TextAsset> ("Assets/Project/CSV/burningHeart.csv") as TextAsset; //ファイル読み込み
+        StringReader reader = new StringReader (csv.text);
+        while (reader.Peek () > -1) {
+            string line = reader.ReadLine ();
+            string[] values = line.Split (',');
+            _timing[notesTotal] = float.Parse (values[0]);
+            _lineNum[notesTotal++] = int.Parse (values[1]);
+        }
+    }
+
+    void CheckNextNotes () {
+        while (_timing[_notesCount] + timeOffset < (Time.time - _startTime) && _timing[_notesCount] != 0) {
+            SpawnNotes (_lineNum[_notesCount]);
+            _notesCount++;
+        }
+    }
+
+    void SpawnNotes (int num) {
+        Instantiate (Note[num],
+            // new Vector3 (-0.7f + (0.35f * num), 8.4f, 4.5f),
+            new Vector3 (-0.676f + (0.338f * num), 8.4f, 4.5f),
+            Quaternion.Euler (-30f, 0, 0));
+    }
 }
