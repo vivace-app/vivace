65dc330890588373c30446187ccfa89265153666
smpny7 <symphonymode7@icloud.com>
2020-07-06 23:44:32 +0900

    [clean] リファクタリング

--- Assets/Project/Scripts/PlayScreen/NotesScript.cs
+++ Assets/Project/Scripts/PlayScreen/NotesScript.cs
@@ -1,12 +1,11 @@
-// using System.Threading.Tasks;
 using System;
-// using System.Collections;
 using UnityEngine;
-// using UnityEngine.SceneManagement;
-// using UnityEngine.UI;
 
 public class NotesScript : MonoBehaviour {
+
+    // -- Temporary Variable. -------------------------------------------------------------
     private float speed = 4.0f;
+    // ------------------------------------------------------------------------------------
 
     void Start () {
         this.transform.localScale -= new Vector3 (0.285f, 0, 0.05f);
@@ -14,8 +13,10 @@ public class NotesScript : MonoBehaviour {
 
     void Update () {
         this.transform.position += (Vector3.down + Vector3.back * (float) Math.Sqrt (3)) * Time.deltaTime * speed;
-        if (this.transform.position.z < -9.3) {
-            Destroy (this.gameObject);
-        }
+        if (this.transform.position.z < -9.3) Destroy (this.gameObject);
+    }
+
+    void OnTriggerEnter () {
+        Debug.Log ("OnTriggerEnter!!");
     }
 }
--- Assets/Project/Scripts/PlayScreen/PlayScreenProcessManager.cs
+++ Assets/Project/Scripts/PlayScreen/PlayScreenProcessManager.cs
@@ -1,26 +1,20 @@
-﻿using System.Threading.Tasks;
+﻿using System.IO;
+using System.Threading.Tasks;
 using UnityEditor;
 using UnityEngine;
-// using UnityEngine.SceneManagement;
-// using UnityEngine.UI;
-// using System;
-using System.IO;
-// using System.Text;
 
 public class PlayScreenProcessManager : MonoBehaviour {
-    // public string csvFilePass = "Assets/CSV/burningHeart.csv";
-    public GameObject[] Note;
     private AudioSource _audioSource;
-
-    private int notesTotal = 0;
+    public GameObject[] Note;
+    private int _notesTotal = 0;
     private int _notesCount = 0;
     private float _startTime = 0;
     public float[] _timing;
     public int[] _lineNum;
 
-    // ---------------------
-    private int timeOffset = 0;
-    // ---------------------
+    // -- Temporary Variable. -------------------------------------------------------------
+        private string csvFilePass = "Assets/Project/CSV/burningHeart.csv";
+    // ------------------------------------------------------------------------------------
 
     async void Start () {
         _timing = new float[1024];
@@ -37,27 +31,21 @@ public class PlayScreenProcessManager : MonoBehaviour {
     }
 
     void LoadCSV () {
-        TextAsset csv = AssetDatabase.LoadAssetAtPath<TextAsset> ("Assets/Project/CSV/burningHeart.csv") as TextAsset; //ファイル読み込み
+        TextAsset csv = AssetDatabase.LoadAssetAtPath<TextAsset> (csvFilePass) as TextAsset;
         StringReader reader = new StringReader (csv.text);
         while (reader.Peek () > -1) {
             string line = reader.ReadLine ();
             string[] values = line.Split (',');
-            _timing[notesTotal] = float.Parse (values[0]);
-            _lineNum[notesTotal++] = int.Parse (values[1]);
+            _timing[_notesTotal] = float.Parse (values[0]);
+            _lineNum[_notesTotal++] = int.Parse (values[1]);
         }
     }
 
     void CheckNextNotes () {
-        while (_timing[_notesCount] + timeOffset < (Time.time - _startTime) && _timing[_notesCount] != 0) {
-            SpawnNotes (_lineNum[_notesCount]);
-            _notesCount++;
-        }
+        while (_timing[_notesCount] < (Time.time - _startTime) && _timing[_notesCount] != 0) SpawnNotes (_lineNum[_notesCount++]);
     }
 
     void SpawnNotes (int num) {
-        Instantiate (Note[num],
-            // new Vector3 (-0.7f + (0.35f * num), 8.4f, 4.5f),
-            new Vector3 (-0.676f + (0.338f * num), 8.4f, 4.5f),
-            Quaternion.Euler (-30f, 0, 0));
+        Instantiate (Note[num], new Vector3 (-0.676f + (0.338f * num), 8.4f, 4.5f), Quaternion.Euler (-30f, 0, 0));
     }
 }
