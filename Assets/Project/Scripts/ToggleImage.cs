7b7f729beb7f46914c3814852513b1a2dc97f4da
smpny7 <symphonymode7@icloud.com>
2020-08-23 15:32:47 +0900

    [upgrade] スワイプメニュー完成

---
+++ Assets/Project/Scripts/ToggleImage.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Toggle))]
+public class ToggleImage : MonoBehaviour
+{
+    public Graphic offGraphic;
+
+    void Start()
+    {
+        Toggle toggle = GetComponent<Toggle>();
+        toggle.onValueChanged.AddListener((value) =>
+        {
+            OnValueChanged(value);
+        });
+        offGraphic.enabled = !toggle.isOn;
+    }
+
+    void OnValueChanged(bool value)
+    {
+        if (offGraphic != null)
+        {
+            offGraphic.enabled = !value;
+        }
+    }
+}
