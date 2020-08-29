b7d287cd8924e2c73b4a8e3d6225972156b8a915
smpny7 <symphonymode7@icloud.com>
2020-08-29 13:00:14 +0900

    ユーザ作成・引き継ぎコード機能実装

--- Assets/Project/Scripts/EnvDataStore.sample.cs
+++ Assets/Project/Scripts/EnvDataStore.sample.cs
@@ -1,5 +1,6 @@
 public static class EnvDataStoreSample
 {
     public static readonly string thisVersion = "0.0.0";
-    public static readonly string licenceApiUrl = "https://api.sample-domain.com/auth-license/";
+    public static readonly string licenceApiUri = "https://sample-domain.com/api/v1/licence";
+    public static readonly string registerApiUri = "https://sample-domain.com/api/v1/register";
 }
