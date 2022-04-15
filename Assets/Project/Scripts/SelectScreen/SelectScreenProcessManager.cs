f7a85193d4c13c4311d83c50dca6ca19a3c01bbb
smpny7 <symphonymode7@icloud.com>
2022-04-16 00:26:06 +0900

    Feat: 他Sceneでの認証情報取得

--- Assets/Project/Scripts/SelectScreen/SelectScreenProcessManager.cs
+++ Assets/Project/Scripts/SelectScreen/SelectScreenProcessManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -9,6 +10,43 @@ namespace Project.Scripts.SelectScreen
     {
         public ToggleGroup toggleGroup;
         public static string selectedLevel;
+
+        private Firebase.Auth.FirebaseAuth _auth;
+        private Firebase.Auth.FirebaseUser _user;
+
+        private void Start() => InitializeFirebase();
+
+        private void InitializeFirebase()
+        {
+            Debug.Log("Setting up Firebase Auth");
+            _auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+            _auth.StateChanged += AuthStateChanged;
+            AuthStateChanged(this, null);
+            
+            var user = _auth.CurrentUser;
+            if (user == null) return;
+            var displayName = user.DisplayName;
+            var userId = user.UserId;
+
+            Debug.Log("name: " + displayName);
+            Debug.Log("uid: " + userId);
+        }
+
+        private void AuthStateChanged(object sender, EventArgs eventArgs)
+        {
+            if (_auth.CurrentUser == _user) return;
+            var signedIn = _user != _auth.CurrentUser && _auth.CurrentUser != null;
+            if (!signedIn && _user != null) Debug.Log("Signed out " + _user.UserId);
+
+            _user = _auth.CurrentUser;
+            if (signedIn) Debug.Log("Signed in " + _user.UserId);
+        }
+
+        private void OnDestroy()
+        {
+            _auth.StateChanged -= AuthStateChanged;
+            _auth = null;
+        }
 
         private static void ScreenTransition()
         {
