1c5d638fd6b9a50b82095c96d58e59a4c35ebfea
smpny7 <git@coalabo.net>
2022-06-12 05:10:40 +0900

    Fix: Sign In With Apple XCode自動化

---
+++ Assets/Editor/SignInWithApplePostprocessor.cs
@@ -0,0 +1,34 @@
+using AppleAuth.Editor;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+
+namespace Editor
+{
+    public static class SignInWithApplePostprocessor
+    {
+#if UNITY_IOS
+        [PostProcessBuild(1)]
+        public static void OnPostProcessBuild(BuildTarget target, string path)
+        {
+            if (target != BuildTarget.iOS)
+                return;
+
+            var projectPath = PBXProject.GetPBXProjectPath(path);
+        
+            // Adds entitlement depending on the Unity version used
+#if UNITY_2019_3_OR_NEWER
+            var project = new PBXProject();
+            project.ReadFromString(System.IO.File.ReadAllText(projectPath));
+            var manager = new ProjectCapabilityManager(projectPath, "Entitlements.entitlements", null, project.GetUnityMainTargetGuid());
+            manager.AddSignInWithAppleWithCompatibility(project.GetUnityFrameworkTargetGuid());
+            manager.WriteToFile();
+#else
+            var manager = new ProjectCapabilityManager(projectPath, "Entitlements.entitlements", PBXProject.GetUnityTargetName());
+            manager.AddSignInWithAppleWithCompatibility();
+            manager.WriteToFile();
+#endif
+        }
+#endif
+    }
+}
