c5b1061dc6768a7af6548356ef03126194df21fb
smpny7 <git@coalabo.net>
2022-06-12 17:37:00 +0900

    Build: APKビルドスクリプトの作成

--- Assets/Editor/SignInWithApplePostprocessor.cs
+++ Assets/Editor/SignInWithApplePostprocessor.cs
@@ -1,3 +1,4 @@
+#if UNITY_IOS
 using AppleAuth.Editor;
 using UnityEditor;
 using UnityEditor.Callbacks;
@@ -7,7 +8,6 @@ namespace Editor
 {
     public static class SignInWithApplePostprocessor
     {
-#if UNITY_IOS
         [PostProcessBuild(1)]
         public static void OnPostProcessBuild(BuildTarget target, string path)
         {
@@ -15,20 +15,22 @@ namespace Editor
                 return;
 
             var projectPath = PBXProject.GetPBXProjectPath(path);
-        
+
             // Adds entitlement depending on the Unity version used
 #if UNITY_2019_3_OR_NEWER
             var project = new PBXProject();
             project.ReadFromString(System.IO.File.ReadAllText(projectPath));
-            var manager = new ProjectCapabilityManager(projectPath, "Entitlements.entitlements", null, project.GetUnityMainTargetGuid());
+            var manager = new ProjectCapabilityManager(projectPath, "Entitlements.entitlements", null,
+                project.GetUnityMainTargetGuid());
             manager.AddSignInWithAppleWithCompatibility(project.GetUnityFrameworkTargetGuid());
             manager.WriteToFile();
 #else
-            var manager = new ProjectCapabilityManager(projectPath, "Entitlements.entitlements", PBXProject.GetUnityTargetName());
+            var manager =
+ new ProjectCapabilityManager(projectPath, "Entitlements.entitlements", PBXProject.GetUnityTargetName());
             manager.AddSignInWithAppleWithCompatibility();
             manager.WriteToFile();
 #endif
         }
-#endif
     }
 }
+#endif
