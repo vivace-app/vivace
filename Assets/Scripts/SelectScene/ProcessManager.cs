22c455632758d85da682a4cd1bf21bb40a9619cf
smpny7 <git@coalabo.net>
2022-07-01 16:18:16 +0900

    Feat: 選曲画面 デザイン実装

---
+++ Assets/Scripts/SelectScene/ProcessManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace SelectScene
+{
+    public class ProcessManager : MonoBehaviour
+    {
+        private void Start()
+        {
+        }
+
+        private void Update()
+        {
+        }
+    }
+}
---
+++ Assets/Scripts/SelectScene/View.cs
@@ -0,0 +1,65 @@
+﻿using Unity.VectorGraphics;
+using UnityEngine;
+
+namespace SelectScene
+{
+    public class View : MonoBehaviour
+    {
+        public static View Instance;
+
+        [SerializeField] private RectTransform artworkBackgroundSvgImage;
+        [SerializeField] private RectTransform artworkBackgroundBottomSvgImage;
+
+        private void Awake()
+        {
+            if (Instance == null) Instance = this;
+        }
+
+        private void Start()
+        {
+            InitializeArtworkBackground();
+        }
+
+        private void InitializeArtworkBackground()
+        {
+            if (Screen.width * 1080 >= Screen.height * 1920)
+            {
+                // 横長スクリーンの場合 
+                Debug.Log("横長");
+                var setBackgroundHeight = -1000f * (Screen.height / 2048f) + 968f;
+                artworkBackgroundSvgImage.sizeDelta =
+                    new Vector2(artworkBackgroundSvgImage.sizeDelta.x, setBackgroundHeight);
+
+                var backgroundPosition = artworkBackgroundSvgImage.anchoredPosition;
+                backgroundPosition.y = -107f - setBackgroundHeight / 2f;
+                artworkBackgroundSvgImage.anchoredPosition = backgroundPosition;
+
+                artworkBackgroundBottomSvgImage.sizeDelta =
+                    new Vector2(setBackgroundHeight + 400f, artworkBackgroundBottomSvgImage.sizeDelta.y);
+
+                var backgroundBottomPosition = artworkBackgroundBottomSvgImage.anchoredPosition;
+                backgroundBottomPosition.y = -107f - setBackgroundHeight - 89.5f;
+                artworkBackgroundBottomSvgImage.anchoredPosition = backgroundBottomPosition;
+            }
+            else
+            {
+                // 縦長スクリーンの場合 
+                Debug.Log("縦長");
+                var setBackgroundHeight = 2439f * (Screen.height / 2048f) - 985.3f;
+                artworkBackgroundSvgImage.sizeDelta =
+                    new Vector2(artworkBackgroundSvgImage.sizeDelta.x, setBackgroundHeight);
+
+                var backgroundPosition = artworkBackgroundSvgImage.anchoredPosition;
+                backgroundPosition.y = -107f - setBackgroundHeight / 2f;
+                artworkBackgroundSvgImage.anchoredPosition = backgroundPosition;
+
+                artworkBackgroundBottomSvgImage.sizeDelta =
+                    new Vector2(setBackgroundHeight + 400f, artworkBackgroundBottomSvgImage.sizeDelta.y);
+
+                var backgroundBottomPosition = artworkBackgroundBottomSvgImage.anchoredPosition;
+                backgroundBottomPosition.y = -107f - setBackgroundHeight - 89.5f;
+                artworkBackgroundBottomSvgImage.anchoredPosition = backgroundBottomPosition;
+            }
+        }
+    }
+}
