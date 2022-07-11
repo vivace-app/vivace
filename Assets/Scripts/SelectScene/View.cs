4d97dadd4896d2cb2ce54c4e8692f0dd30d28c26
smpny7 <git@coalabo.net>
2022-07-11 21:59:41 +0900

    Fix: 選曲画面 レスポンシブ修正

--- Assets/Scripts/SelectScene/View.cs
+++ Assets/Scripts/SelectScene/View.cs
@@ -1,5 +1,4 @@
-﻿using Unity.VectorGraphics;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace SelectScene
 {
@@ -7,8 +6,9 @@ namespace SelectScene
     {
         public static View Instance;
 
-        [SerializeField] private RectTransform artworkBackgroundSvgImage;
-        [SerializeField] private RectTransform artworkBackgroundBottomSvgImage;
+        [SerializeField] private RectTransform artworkBackgroundRectTransform;
+        [SerializeField] private RectTransform artworkBackgroundBottomRectTransform;
+        [SerializeField] private RectTransform canvasRectTransform;
 
         private void Awake()
         {
@@ -22,45 +22,15 @@ namespace SelectScene
 
         private void InitializeArtworkBackground()
         {
-            if (Screen.width * 1080 >= Screen.height * 1920)
-            {
-                // 横長スクリーンの場合 
-                Debug.Log("横長");
-                var setBackgroundHeight = -900f * (Screen.height / 2048f) + 920f;
-                artworkBackgroundSvgImage.sizeDelta =
-                    new Vector2(artworkBackgroundSvgImage.sizeDelta.x, setBackgroundHeight);
+            var artworkBackgroundHeight = canvasRectTransform.sizeDelta.y - 700;
+            artworkBackgroundRectTransform.sizeDelta = new Vector2(artworkBackgroundRectTransform.sizeDelta.x,
+                artworkBackgroundHeight * 0.7f);
+            artworkBackgroundBottomRectTransform.sizeDelta =
+                new Vector2(artworkBackgroundHeight + 300, artworkBackgroundHeight * 0.3f);
 
-                var backgroundPosition = artworkBackgroundSvgImage.anchoredPosition;
-                backgroundPosition.y = -107f - setBackgroundHeight / 2f;
-                artworkBackgroundSvgImage.anchoredPosition = backgroundPosition;
-
-                var setBackgroundBottomHeight = setBackgroundHeight * 0.45f;
-                artworkBackgroundBottomSvgImage.sizeDelta =
-                    new Vector2(setBackgroundBottomHeight * 5f, setBackgroundBottomHeight);
-
-                var backgroundBottomPosition = artworkBackgroundBottomSvgImage.anchoredPosition;
-                backgroundBottomPosition.y = -107f - setBackgroundHeight - setBackgroundBottomHeight / 2f;
-                artworkBackgroundBottomSvgImage.anchoredPosition = backgroundBottomPosition;
-            }
-            else
-            {
-                // 縦長スクリーンの場合 
-                Debug.Log("縦長");
-                var setBackgroundHeight = 2439f * (Screen.height / 2048f) - 985.3f;
-                artworkBackgroundSvgImage.sizeDelta =
-                    new Vector2(artworkBackgroundSvgImage.sizeDelta.x, setBackgroundHeight);
-
-                var backgroundPosition = artworkBackgroundSvgImage.anchoredPosition;
-                backgroundPosition.y = -107f - setBackgroundHeight / 2f;
-                artworkBackgroundSvgImage.anchoredPosition = backgroundPosition;
-
-                artworkBackgroundBottomSvgImage.sizeDelta =
-                    new Vector2(setBackgroundHeight + 400f, artworkBackgroundBottomSvgImage.sizeDelta.y);
-
-                var backgroundBottomPosition = artworkBackgroundBottomSvgImage.anchoredPosition;
-                backgroundBottomPosition.y = -107f - setBackgroundHeight - 89.5f;
-                artworkBackgroundBottomSvgImage.anchoredPosition = backgroundBottomPosition;
-            }
+            var bottomPosition = artworkBackgroundBottomRectTransform.anchoredPosition;
+            bottomPosition.y -= artworkBackgroundHeight * 0.7f - (497f - 106.8f);
+            artworkBackgroundBottomRectTransform.anchoredPosition = bottomPosition;
         }
     }
 }
