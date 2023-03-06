8f1e825e1f17268412178e39ca39c337993c0782
smpny7 <58544849+smpny7@users.noreply.github.com>
2023-03-07 02:46:14 +0900

    Revert "Build: PostBuildProcess to One File"
    This reverts commit cbe7add28c9b05142cad7d689596e037d5e1c836.

--- Assets/Library/Editor/PostBuildProcess.cs
+++ Assets/Library/Editor/PostBuildProcess.cs
@@ -1,6 +1,5 @@
 #if UNITY_IOS
 using System.IO;
-using System.Text.RegularExpressions;
 using UnityEditor;
 using UnityEditor.Callbacks;
 using UnityEditor.iOS.Xcode;
@@ -28,15 +27,6 @@ namespace Library.Editor
             pj.SetBuildProperty(target, "ENABLE_BITCODE", "NO");
 
             File.WriteAllText(pjPath, pj.WriteToString());
-            
-            var projectPath = path + "/Unity-iPhone.xcodeproj/project.pbxproj";
-            //UNITY 2022.2.X FIX https://forum.unity.com/threads/xcode-build-error-after-upgrading-to-2022-2-0.1371966/
-            var projRaw = File.ReadAllText(projectPath);
-            projRaw = projRaw.Replace("chmod +x \\\"$IL2CPP\\\"",
-                "chmod -R +x *\\nchmod +x \\\"$IL2CPP\\\"");
-            projRaw = Regex.Replace(projRaw, "--data-folder=\\\\\"([^\"]*)\\\\\"",
-                "--data-folder=\\\"$PROJECT_DIR/Data/Managed\\\"");
-            File.WriteAllText(projectPath, projRaw);
         }
     }
 }
